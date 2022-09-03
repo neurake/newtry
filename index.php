7a729809a32d1cc570d582459a31a28f16aa3478
tukaramkitekar <tukaramkitekar401@outlook.com>
2022-09-03 11:10:50 +0530

    intial files

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/fontawesome/css/all.css">
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico">
+    <title>Newtry</title>
+    <style>
+          *{margin:0; padding:0;}
+          nav{background:black; height:65px; width:100%;}
+          .navleft{float:left; width:20%;}
+          .navcenter{float:left; width:65%;}
+          .navright{float:right; width:15%;}
+          ul{margin:20px; margin-inline:60px; margin-top:15px;}
+          ul li{display:inline-block; margin:0 20px;}
+          ul li a{text-decoration:none; list-style:none; color:white; font-size:20px; font-family:Dubai;}
+         
+          .firstdiv{width:100%; height:500px;}
+          .firstdiv1{float:left; width:60%;}
+          .firstdiv2{float:right; width:40%;}
+    </style>
+</head>
+
+
+
+<body>
+<nav>
+    <div class="navleft">
+    <img src="assets/logo.png" width="150px" style="margin-inline:40px; margin-top:4px;">
+</div>
+
+<div class="navcenter">
+    <ul>
+     
+        <li><a href="#"><i class="fa-solid fa-mobile" style="color:white"></i> Mobiles</a></li>
+        <li><a href="#"><i class="fa-solid fa-computer" style="color:white"></i> Computers</a></li>
+        <li><a href="#"><i class="fa-solid fa-radio" style="color:white"></i> Speakers</a></li>
+        <li><a href="#"><i class="fa-regular fa-clock" style="color:white"></i> Watch</a></li>
+       
+        <li><a href="#"><i class="fa-solid fa-book" style="color:white"></i> Books</a></li>
+    
+        
+</ul>
+</div>
+
+<div class="navright">
+<h2><i class="fa-solid fa-magnifying-glass" style="color:white; margin-top:20px;"></i><i class="fa-solid fa-cart-shopping" style="color:white; margin-inline:30px"></i>  <a href="user/signin.php"> <i class="fa-regular fa-user" style="color:white; margin-inline:7px;"></i></a></h2>
+</div>
+
+
+
+
+</nav>  
+
+<main>
+<div class="firstdiv">
+                   
+</div>
+</main>
+</body>
+</html>
---
+++ user/signin.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./assets/fontawesome/css/all.css">
+    <link rel="icon" type="image/x-icon" href="assets/favicon.ico">
+    <title>Newtry</title>
+    <style>
+          *{margin:0; padding:0;}
+          nav{background:black; height:65px; width:100%;}
+         
+          
+    </style>
+</head>
+
+
+
+
+<body>
+<nav>
+    
+   <center> <img src="../assets/logo.png" width="150px" style="margin-inline:0px; margin-top:4px;"> </center>
+
+
+
+
+
+
+</nav>
+
+<form>
+     
+</form>
+</body>
+</html>
